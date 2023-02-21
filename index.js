8ea7d1c0015dd37fcab5a75356c310fc0c24133b
Brianne <92082302+briannededeugd@users.noreply.github.com>
2023-02-21 12:25:59 +0100

    Begin week 2 werk voor feedback

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
-const app = express()
-const PORT = process.env.PORT || 8080
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Met express.static zeg ik dat mijn server alle statische files mag gebruiken in de directory.
 // Daarna zeg ik dat hij alle bestanden in de css directory mag gebruiken.
-app.use(express.static(path.join(__dirname, 'matching-application')));
-app.use('/css', express.static(path.join(__dirname, 'matching-application/css'))); // Toch wordt mijn css niet geladen.
-app.use('/img', express.static(path.join(__dirname, 'matching-application/img'))); // Zou dit werken voor img (update: NEE)
+app.use(express.static("static"));
 
 
 // req staat voor 'request', dus wat de HTTP request is, en res staat voor 'response' wat een express app terugstuurt 
@@ -37,8 +35,8 @@ function onMuziekelement(req, res) {
     res.sendFile(path.join(__dirname, 'view/muziekelement.html'));
 };
 
-app.listen(PORT, () => console.log('Running on port: 8080'))
-
 app.get('*', function(req, res) {
   res.status(404).send('Page not found');
 });
+
+app.listen(PORT, () => console.log('Running on port: 8080'));
