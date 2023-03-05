11562cdb454d2a6e116eb31e1660e33102156aec
Brianne <92082302+briannededeugd@users.noreply.github.com>
2023-03-05 17:38:58 +0100

    Gebruik EJS, linters, formatters, build tools en MongoDB

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 console.log("Hello");
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const __dirname = "matching-application";
+
 // Met express.static zeg ik dat mijn server alle statische files mag gebruiken in de directory
-app.set('views', path.join(__dirname, 'view'))
-app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "view"));
+app.set("view engine", "ejs");
 app.use(express.static("static"));
 
-const fs = require('fs');
+const fs = require("fs");
 
 // Read the contents of your JSON file
-const jsonData = fs.readFileSync('static/data/songs.json', 'utf8');
+const jsonData = fs.readFileSync("static/data/songs.json", "utf8");
 
 // Parse the JSON data into an array of objects
 const myData = JSON.parse(jsonData);
@@ -23,69 +25,46 @@ const randomIndex = Math.floor(Math.random() * myData.length);
 
 // Get the title and mood from the random object
 const title = myData[randomIndex].title;
-const mood = myData[randomIndex].mood;
 
-
-
-
-
-
-// req staat voor 'request', dus wat de HTTP request is, en res staat voor 'response' wat een express app terugstuurt 
-app.get('/', onHome)
+// req staat voor 'request', dus wat de HTTP request is, en res staat voor 'response' wat een express app terugstuurt
+app.get("/", onHome);
 
 function onHome(req, res) {
-    // res.send('Hallo');
-    res.sendFile(path.join(__dirname, 'view/index.html'));
+	// res.send('Hallo');
+	res.sendFile(path.join(__dirname, "view/index.html"));
 }
-
 
 // Formulierpagina om gevoelens te kiezen
-app.get('/voelen', onVoelen)
+app.get("/voelen", onVoelen);
 
 function onVoelen(req, res) {
-    res.render('voelen');
+	res.render("voelen");
 }
-  
-
-
 
 // Formulier om muziekelement te kiezen
-app.get('/muziekelement', onMuziekelement)
+app.get("/muziekelement", onMuziekelement);
 
 function onMuziekelement(req, res) {
-    res.render('muziekelement');
-};
-
-
-
+	res.render("muziekelement");
+}
 
 // Formulier om taalvoorkeur te kiezen
-app.get('/taal', onTaal)
+app.get("/taal", onTaal);
 
 function onTaal(req, res) {
-    res.render('taal');
-};
-
+	res.render("taal");
+}
 
 // Resultaatpagina
-app.get('/resultaat', onResult)
+app.get("/resultaat", onResult);
 
 function onResult(req, res) {
-    res.render('resultaat', {title: myData[1].title, artist: myData[1].artist}),
-    console.log({title});
-};
+	res.render("resultaat", { title: myData[3].title, artist: myData[3].artist }),
+		console.log({ title });
+}
 
-app.get('*', function(req, res) {
-  res.status(404).send('Page not found');
+app.get("*", function (req, res) {
+	res.status(404).send("Page not found");
 });
 
-
-
-
-
-
-
-
-
-
-app.listen(PORT, () => console.log('Running on port: 8080'));
+app.listen(PORT, () => console.log("Running on port: 8080"));
